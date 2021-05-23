659f08da94eab0a9a18ace7fbbe569ac5fffb746
andddias <and.ddias@gmail.com>
2021-05-23 11:59:12 -0300

    Project created

---
+++ backend/src/test/java/com/andredias/apiclients/ApiclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andredias.apiclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
